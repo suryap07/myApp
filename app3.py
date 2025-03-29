dd9a64e5dd6177f4255e72e073bd398b334e69f6
surya <surya.jshukla>
2025-03-29 07:49:01 +0000

    Add two number program

---
+++ app3.py
@@ -0,0 +1,8 @@
+#input two number
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enthe the second number: "))
+
+# Add the number
+
+sum_result = num1 + num2
+print("The sum is:", sum_result)
